66a3f881fb20a9170d3bf1ef07c2d14de9bceba0
amurlin <e.amurlin@gmail.com>
2024-12-03 12:35:19 +0800

    g

--- javascript.js
+++ javascript.js
@@ -1,81 +1,232 @@
-// Problem 1
-let a = 4, b = 3, c = 5
-console.log(a+b+c)
+// // Problem 1
+// let a = 4, b = 3, c = 5
+// console.log(a+b+c)
 
-// problem 2 
-let d = 12, e =23 
-console.log(d+e)
+// // problem 2 
+// let d = 12, e =23 
+// console.log(d+e)
 
-// problem 3
-let f = 5
-console.log(f*f*f, f*f*6)
+// // problem 3
+// let f = 5
+// console.log(f*f*f, f*f*6)
 
-//  problem 4
-let g = 6, h=4 
-console.log(g*h, g*2+h*2)
+// //  problem 4
+// let g = 6, h=4 
+// console.log(g*h, g*2+h*2)
 
-// problem 5
-let x= 1
-console.log(y=3*x-5)
+// // problem 5
+// let x= 1
+// console.log(y=3*x-5)
 
-// problem 6
-let j= -2
-console.log(k=4*j*j-3*j+5)
+// // problem 6
+// let j= -2
+// console.log(k=4*j*j-3*j+5)
 
-// problem 7
-let l= 476
-console.log(l%10)
+// // problem 7
+// let l= 476
+// console.log(l%10)
 
-// problem 8 
-let p= 423
-let i= p%100-3
-console.log(i/10)
+// // problem 8 
+// let p= 423
+// let i= p%100-3
+// console.log(i/10)
 
-// problem 9
-let w= 27
-let z= Math.floor(w/10)
-let m= w%10
-console.log(z*m)
+// // problem 9
+// let w= 27
+// let z= Math.floor(w/10)
+// let m= w%10
+// console.log(z*m)
 
-// problem 10 
-let ons= 123
-let o= Math.floor(ons/100)
-let n= (ons%100-3)/10
-let s= ons%10
-console.log(o+n+s)
+// // problem 10 
+// let ons= 123
+// let o= Math.floor(ons/100)
+// let n= (ons%100-3)/10
+// let s= ons%10
+// console.log(o+n+s)
 
-// problem 11
-let sec= 200
-console.log(Math.floor(sec/60), sec%60 )
+// // problem 11
+// let sec= 200
+// console.log(Math.floor(sec/60), sec%60 )
 
-// problem 12
-let mmm= 3612
-console.log(Math.floor(mmm/60/60), mmm%60-12, mmm%60)
+// // problem 12
+// let mmm= 3612
+// console.log(Math.floor(mmm/60/60), mmm%60-12, mmm%60)
 
-// problem 13
-let min2= 3
-let sec2= 4
-console.log(min2*60+sec2)
+// // problem 13
+// let min2= 3
+// let sec2= 4
+// console.log(min2*60+sec2)
 
-// // problem 14
-// let hon= 1; 2; 3
-// console.log(hon*24+hon*60+hon)
+// // // problem 14
+// // let hon= 1; 2; 3
+// // console.log(hon*24+hon*60+hon)
 
-// problem 15
-let tsag= 44
-console.log(Math.floor(tsag/24), tsag%24)
+// // problem 15
+// let tsag= 44
+// console.log(Math.floor(tsag/24), tsag%24)
 
-// problem 16
-let days= 2 
-let hours= 5
-console.log(days*24+hours)
+// // problem 16
+// let days= 2 
+// let hours= 5
+// console.log(days*24+hours)
 
-// problem 17
-let sar= 27
-console.log(Math.floor(sar/12), sar%12)
+// // problem 17
+// let sar= 27
+// console.log(Math.floor(sar/12), sar%12)
 
-// problem 18
-let year= 2
-let month= 7
-console.log
+// // problem 18
+// let year= 2
+// let month= 7
+
+// let test = 2^5
+// console.log(test);
+
+// console.log(2*12+7)
+
+// 12.04 ----------------------------------------------
+
+// test
+// let age =17
+// if (age >= 18) {
+//     console.log("tanii nas", age);
+//     console.log("ta nasand hursen baina.");
+// } else {
+//     console.log("ta huuhdeere baina.");
+// }
+
+// 1.Hoyr toonii ih
+// let too1= 16;
+// let too2= 20;
+
+// if (too1>too2) {
+//     console.log(too1, "ih too")
+// } else {
+//     console.log(too2, "ih too")
+    
+// }
+
+// 2. Hoyr toonii baga
+// let too1= 9;
+// let too2= 4;
+
+// if (too1>too2) {
+//     console.log(too2, 'baga too')
+// } else {
+//     console.log(too1, 'baga too')
+// }
+
+// 3. 3 toonii ih
+// let too1= 1;
+// let too2= 3;
+// let too3= 9;
+
+// if (too1>too2>too3) {
+//      console.log(too1, "ih too")
+// } else if (too1<too2>too3) {
+//     console.log(too2, "ih too")
+// } else {
+//     console.log(too3, "ih too")
+// }
+
+// 4. 4 toonii baga
+// let too1= 40;
+// let too2= 10;
+// let too3= 46;
+// let too4= 50;
+
+// if (too1>too2>too3>too4) {
+//     console.log(too4, 'baga too')
+// } 
+// if (too1>too2>too3<too4) {
+//     console.log(too3, 'baga too')
+// } 
+// if (too1>too2<too3<too4) {
+//     console.log(too2, 'baga too')
+// } 
+// if (too1<too2<too3<too4){
+//     console.log(too1, 'baga too')
+// }
+
+// if (too1>too2>too3) {
+//     if (too3>too4)
+//         console.log(too4,'baga too');
+// } else if (too2>too3<too4){
+//     if (too1>too3)
+//         console.log(too3, 'baga too'); 
+// } else if (too1>too2<too3){
+//     if ( too2<too4)
+//         console.log(too2, "baga too");
+// } else {
+//     console.log(too1, 'baga too');
+// }
+
+// if (too1<too2) {
+//     if (too1<too3);
+//     if (too1<too4)
+//         console.log(too1, 'baga too');
+// } else if (too1>too2) {
+//     if (too2<too3);
+//     if (too2<too4)
+//         console.log(too2, 'baga too');
+// } else if (too2>too3) {
+//     if (too1>too3);
+//     if (too3<too4)
+//         console.log(too3, 'baga too');
+// } else  {
+//     if (too1>too4);
+//     if (too3>too4)
+//         console.log(too4, 'baga too');
+// }
+
+// 5. Niilber 80
+// let too1 = 85;
+// let too2 = 75;
+// let too3 = 96;
+// let too4 = 69;
+
+// if (too1>80) {
+//     if (too2>80)
+//         console.log(too1+too2); 
+//     if (too3>80)
+//         console.log(too1+too3); 
+//     if (too4>80)
+//         console.log(too1+too4);           
+// }
+// if (too2>80) {
+//     if (too3>80)
+//         console.log(too2+too3); 
+//     if (too4>80)
+//         console.log(too2+too4); 
+// }
+
+// if (too3>80) {  
+//     if (too4>80)
+//         console.log(too3+too4);
+// }
+
+// 6. Urjver 5
+
+let too1= 3;
+let too2= 6;
+let too3= 2;
+let too4= 4;
+
+if (too1<5){
+    if (too2<5)
+        console.log(too1*too2);
+    if (too3<5)
+        console.log(too1*too3);
+    if (too4<5)
+        console.log(too1*too4);           
+}
+if (too2<5) {
+    if (too3< 5)
+        console.log(too2*too3);
+    if (too4<5)
+        console.log(too2*too4); 
+}
+if (too3<5) {
+    if (too4<5)
+        console.log(too3*too4);
+}
 
